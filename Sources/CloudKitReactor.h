7bfec61d31821495a73624cff7eccebcf3e3d187
Ben Norris <code@bsn.design>
2017-01-18 08:47:11 -0700

    Add empty framework

---
+++ Sources/CloudKitReactor.h
@@ -0,0 +1,19 @@
+//
+//  CloudKitReactor.h
+//  CloudKitReactor
+//
+//  Created by Ben Norris on 1/18/17.
+//  Copyright © 2017 BSN Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CloudKitReactor.
+FOUNDATION_EXPORT double CloudKitReactorVersionNumber;
+
+//! Project version string for CloudKitReactor.
+FOUNDATION_EXPORT const unsigned char CloudKitReactorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CloudKitReactor/PublicHeader.h>
+
+
