e571528c18c174080fef0a0449323899cd898a8c
Ben Norris <code@bsn.design>
2017-02-02 17:34:48 -0700

    Add completion handler when fetching single record

--- Sources/FetchCloudKitRecord.swift
+++ Sources/FetchCloudKitRecord.swift
@@ -12,11 +12,13 @@ import CloudKit
 public struct FetchCloudKitRecord<T: CloudKitSyncable, U: State>: Command {
     
     public var record: T
+    public var completion: (() -> Void)?
     public var privateDatabase: Bool
     
-    public init(for record: T, privateDatabase: Bool = true) {
+    public init(for record: T, privateDatabase: Bool = true, completion: (() -> Void)? = nil) {
         self.record = record
         self.privateDatabase = privateDatabase
+        self.completion = completion
     }
     
     public func execute(state: U, core: Core<U>) {
@@ -33,6 +35,7 @@ public struct FetchCloudKitRecord<T: CloudKitSyncable, U: State>: Command {
     }
     
     private func process(_ record: CKRecord?, error: Error?, state: U, core: Core<U>) {
+        defer { completion?() }
         if let error = error {
             core.fire(event: CloudKitRecordFetchError(error: error))
         } else if let record = record {
