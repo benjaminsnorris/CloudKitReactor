c9bef7e04987659cc748d95b581459e82440eabd
Ben Norris <code@bsn.design>
2017-06-12 21:51:34 -0600

    Add support for fetching record zone

---
+++ Sources/FetchRecordZone.swift
@@ -0,0 +1,35 @@
+/*
+ |  _   ____   ____   _
+ | | |‾|  ⚈ |-| ⚈  |‾| |
+ | | |  ‾‾‾‾| |‾‾‾‾  | |
+ |  ‾        ‾        ‾
+ */
+
+import Foundation
+import Reactor
+import CloudKit
+
+public struct FetchRecordZone<U: State>: Command {
+    
+    public var zoneID: CKRecordZoneID
+    public var completion: ((Bool) -> Void)?
+    
+    public init(with zoneID: CKRecordZoneID, completion: ((Bool) -> Void)? = nil) {
+        self.zoneID = zoneID
+        self.completion = completion
+    }
+    
+    public func execute(state: U, core: Core<U>) {
+        let operation = CKFetchRecordZonesOperation(recordZoneIDs: [zoneID])
+        operation.fetchRecordZonesCompletionBlock = { zonesDictionary, error in
+            if let error = error {
+                core.fire(event: CloudKitOperationUpdated(status: .errored(error), type: .fetch))
+            } else {
+                let found = zonesDictionary?[self.zoneID] != nil
+                core.fire(event: CloudKitOperationUpdated(status: .completed, type: .fetch))
+                self.completion?(found)
+            }
+        }
+    }
+    
+}
