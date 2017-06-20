74941e2a0b40f54ccda0446209cdd9eda2f41452
Ben Norris <code@bsn.design>
2017-06-19 23:21:33 -0600

    Support subscription ID in query subscription

--- Sources/SubscribeToCloudKit.swift
+++ Sources/SubscribeToCloudKit.swift
@@ -14,10 +14,11 @@ public struct SubscribeToCloudKit<T: CloudKitSyncable, U: State>: Command {
     public var predicate: NSPredicate
     public var options: CKQuerySubscriptionOptions
     public var notificationInfo: CKNotificationInfo
+    public var subscriptionID: String?
     public var privateDatabase: Bool
     public var zoneID: CKRecordZoneID
 
-    public init(predicate: NSPredicate = NSPredicate(value: true), options: CKQuerySubscriptionOptions = [.firesOnRecordCreation, .firesOnRecordUpdate, .firesOnRecordDeletion], notificationInfo: CKNotificationInfo? = nil, privateDatabase: Bool = true, zoneID: CKRecordZoneID = CloudKitReactorConstants.zoneID) {
+    public init(predicate: NSPredicate = NSPredicate(value: true), options: CKQuerySubscriptionOptions = [.firesOnRecordCreation, .firesOnRecordUpdate, .firesOnRecordDeletion], notificationInfo: CKNotificationInfo? = nil, subscriptionID: String? = nil, privateDatabase: Bool = true, zoneID: CKRecordZoneID = CloudKitReactorConstants.zoneID) {
         self.predicate = predicate
         self.options = options
         if let notificationInfo = notificationInfo {
@@ -26,12 +27,18 @@ public struct SubscribeToCloudKit<T: CloudKitSyncable, U: State>: Command {
             self.notificationInfo = CKNotificationInfo()
             notificationInfo?.shouldSendContentAvailable = true
         }
+        self.subscriptionID = subscriptionID
         self.privateDatabase = privateDatabase
         self.zoneID = zoneID
     }
     
     public func execute(state: U, core: Core<U>) {
-        let subscription = CKQuerySubscription(recordType: T.recordType, predicate: predicate, options: options)
+        let subscription: CKQuerySubscription
+        if let subscriptionID = subscriptionID {
+            subscription = CKQuerySubscription(recordType: T.recordType, predicate: predicate, subscriptionID: subscriptionID, options: options)
+        } else {
+            subscription = CKQuerySubscription(recordType: T.recordType, predicate: predicate, options: options)
+        }
         subscription.notificationInfo = notificationInfo
         subscription.zoneID = zoneID
         
