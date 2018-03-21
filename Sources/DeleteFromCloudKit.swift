c4b14f10e31830f5d7e3456315810572fe9bb67c
Ben Norris <code@bsn.design>
2018-03-21 09:43:55 -0600

    Add completion handler to delete call

--- Sources/DeleteFromCloudKit.swift
+++ Sources/DeleteFromCloudKit.swift
@@ -14,27 +14,29 @@ public struct DeleteFromCloudKit<U: State>: Command {
     public var objects: [CloudKitSyncable]
     public var records: [CKRecord]
     public var databaseScope: CKDatabaseScope
+    let completion: (() -> Void)?
     
-    public init(_ objects: [CloudKitSyncable] = [], records: [CKRecord] = [], databaseScope: CKDatabaseScope = .private) {
+    public init(_ objects: [CloudKitSyncable] = [], records: [CKRecord] = [], databaseScope: CKDatabaseScope = .private, completion: (() -> Void)? = nil) {
         self.objects = objects
         self.records = records
         self.databaseScope = databaseScope
+        self.completion = completion
     }
     
-    public init(_ object: CloudKitSyncable? = nil, record: CKRecord? = nil, databaseScope: CKDatabaseScope = .private) {
+    public init(_ object: CloudKitSyncable? = nil, record: CKRecord? = nil, databaseScope: CKDatabaseScope = .private, completion: (() -> Void)? = nil) {
         if let object = object {
-            self.init([object], databaseScope: databaseScope)
+            self.init([object], databaseScope: databaseScope, completion: completion)
         } else if let record = record {
-            self.init(records: [record], databaseScope: databaseScope)
+            self.init(records: [record], databaseScope: databaseScope, completion: completion)
         } else {
-            self.init([])
+            self.init([], completion: completion)
         }
     }
     
     public func execute(state: U, core: Core<U>) {
         var recordIDs = objects.flatMap { $0.cloudKitRecordID }
         recordIDs += records.map { $0.recordID }
-        guard !recordIDs.isEmpty else { return }
+        guard !recordIDs.isEmpty else { completion?(); return }
         let operation = CKModifyRecordsOperation(recordsToSave: nil, recordIDsToDelete: recordIDs)
         operation.savePolicy = .ifServerRecordUnchanged
         operation.modifyRecordsCompletionBlock = { _, _, error in
@@ -43,6 +45,7 @@ public struct DeleteFromCloudKit<U: State>: Command {
             } else {
                 core.fire(event: CloudKitOperationUpdated(status: .completed, type: .delete))
             }
+            self.completion?()
         }
         operation.qualityOfService = .userInitiated
         
